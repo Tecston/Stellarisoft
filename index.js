5d88b198880bb20e75c9e7b4223883b1ba259d06
Tecston <jrmedver2004@gmail.com>
2024-03-30 00:26:53 -0700

    sss

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console
+const { Client } = require ('pg');
+    
+const obtenerArboles = async () => {
+    const client = new Client({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'Arboles',
+        password: 'medina',
+        port: 5432,
+    });
+    
+    
+    await client.connect()
+    
+    const res = await client.query('SELECT * FROM arboles;')
+
+    const result = res.rows[0].message;
+
+    await client.end()
+    
+    return result;
+};
+
+obtenerArboles().then((result) => {
+    console.log(result);
+});
